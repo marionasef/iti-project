b4f203b1108746a818330016230e7a29c99f737b
marionasef100 <mario100noca@gmail.com>
2024-09-20 20:39:31 +0300

    aboutus page update

--- construction-website/src/components/Topimage.jsx
+++ construction-website/src/components/Topimage.jsx
@@ -5,14 +5,13 @@ export default function Topimage(props) {
     <>
       <section
         className={`top-image d-flex`}
-        style={{ backgroundImage: `url(${props.image})` }}
-      >
-        <div className="container">
+        style={{ backgroundImage: `url(${props.image})` }}>
+        <div className="container position-relative">
           <div className="top-text">
             <h2 className="fs-1">{props.text1}</h2>
             <p className="fs-5 fw-light">{props.text2}</p>
           </div>
-          <div className="contact-card shadow p-3 mb-5 bg-body-tertiary">
+          <div className="contact-card shadow p-3 mb-5 bg-body-tertiary position-absolute">
             <div className="p-2 ps-4">
               <p className="cont-info">
                 Call Us Today
--- construction-website/src/pages/aboutus/AboutUs.jsx
+++ construction-website/src/pages/aboutus/AboutUs.jsx
@@ -1,12 +1,64 @@
+import "./about.css";
 import React from "react";
 import Navigation from "../../components/Navigation";
 import Topimage from "../../components/Topimage";
 import heroImage from "../../assets/about.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faPlusSquare } from "@fortawesome/free-solid-svg-icons";
+import Footer from "../../components/Footer";
+
 export default function AboutUs() {
   return (
     <>
       <Navigation />
       <Topimage text1="AboutUs" text2="Home/AboutUs" image={heroImage} />
+      <section className="first-section">
+        <div className=" container d-flex  gap-4 align-items-center justify-content-center">
+          <div class="position-relative">
+            <img
+              className=" w-75"
+              src={require("../../assets/Rectangle 25.png")}
+              alt="Construction Worker"
+            />
+            <div class="little-card ">
+              <h2>+ 25</h2>
+              <span>Years of expirience</span>
+            </div>
+          </div>
+          <div className=" w-50  mt-4">
+            <h4 className=" mb-4 text-danger">AboutUs</h4>
+            <p className=" fw-bold text-black fs-5 mt-4 d-block">
+              We ‘re providing the best customer service
+            </p>
+            <p className=" w-75 bg-body-secondary text-black p-3">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. <br />
+              <h3 className=" pt-3">John Doe, CEO</h3>
+            </p>
+            <button className="btn btn-danger my-3 p-3 px-5">
+              Learn more <FontAwesomeIcon icon={faArrowRight} />
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <section className="second-section sorakbira">
+        <div className=" section-comp d-flex flex-column justify-content-center align-items-center ">
+          <p className=" fw-light fs-2">Get a Quote</p>
+          <p className=" fw-bold fs-5 ">Build Your Future Today</p>
+          <p className=" fw-light w-50">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's
+          </p>
+          <button className="btn btn-danger my-3 p-3 px-5">
+            Appointment <FontAwesomeIcon icon={faArrowRight} />
+          </button>
+        </div>
+      </section>
+
+      <Footer />
     </>
   );
 }
